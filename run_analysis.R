306a55bb3744542a31282f551df8fc429279436b
teomoreno <teo.moreno@gmail.com>
2015-09-25 20:18:56 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,32 +19,34 @@ x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 
-View(subject_train)
-
 # 01 Merging the Training and the Test Sets to Create One Data Set
 
-## Adding Variable Names, SubjectNumber and ActivityType Labeled Columns to Both Sets
+## Adding Column Names to Activity Training and Test Data Frames
+
+colnames(y_train) <- c("Activity")
+colnames(y_test) <- c("Activity")
+
+## Adding Column Names to Participant (Subject) Training and Test Data Frames
+
+colnames(subject_train) <- c("ParticipantNumber")
+colnames(subject_test) <- c("ParticipantNumber")
+
+## Adding Column Names to Measurements Training and Test Data Frames
 
 colnames(x_train) <- t(features[2])
-colnames(subject_train) <- c("ParticipantNumber")
-colnames(y_train) <- c("Activity")
-x_train <- cbind(y_train, x_train)
-x_train <- cbind(subject_train, x_train)
-
 colnames(x_test) <- t(features[2])
-colnames(subject_test) <- c("ParticipantNumber")
-colnames(y_test) <- c("Activity")
-x_test <- cbind(y_test, x_test)
+
+## Merging Activity, Participant and Measurements Data Frames
+
+x_train <- cbind(subject_train, x_train)
+x_train <- cbind(y_train, x_train)
+
 x_test <- cbind(subject_test, x_test)
+x_test <- cbind(y_test, x_test)
 
-View(subject_train)
-View(y_train)
-
-## Merging Sets to Create One
+## Merging Training and Test Sets to Create One
 
 merged <- rbind(x_train, x_test)
-
-View(merged)
 
 # 02 Extracting Only the Measurements on the Mean and Standard Deviation for Each Measurement
 
@@ -58,9 +60,6 @@ mean_ext <- grep("mean()", names(merged), value = FALSE)
 
 measurements_mean <- merged[mean_ext]
 
-View(mean_ext)
-View(measurements_mean)
-
 ## Extracting Standard Deviation Measurements
 
 ### Extracting column numbers for standard deviation measurements
@@ -70,8 +69,6 @@ std_ext <- grep("std()", names(merged), value = FALSE)
 ### Subsetting standard deviation measurements using using column numbers
 
 measurements_std <- merged[std_ext]
-
-View(measurements_std)
 
 # 03 Using Descriptive Activity Names to Name the Activities in the Data Set
 
@@ -84,11 +81,9 @@ merged$Activity[merged$Activity == 4] <- c("Sitting")
 merged$Activity[merged$Activity == 5] <- c("Standing")
 merged$Activity[merged$Activity == 6] <- c("Laying")
 
-View(merged)
-
 # 04 Appropriately Labelling the Data Set with Descriptive Variable Names
 
-## Replacing Abbreviations with Full Words in the Continuos Variables Names
+## Replacing Abbreviations with Full Words for the Continuous Variables Names
 
 names(merged) <- gsub("Acc", "Accelerometer", names(merged))
 names(merged) <- gsub("Mag", "Magnitude", names(merged))
@@ -96,20 +91,15 @@ names(merged) <- gsub("Gyro", "Gyroscope", names(merged))
 names(merged) <- gsub("^t", "time", names(merged))
 names(merged) <- gsub("^f", "frequency", names(merged))
 
-View(merged)
-str(merged)
-
 # 05 Creating an Independent Tidy Data Set with the Average of Each Variable by Activity and Subject
 
 ## Summarizing Data Set: Calculating Variable Averages.  
 
 tidy_data <-aggregate(. ~ Activity + ParticipantNumber, merged, mean)
 
-## Sorting the Summary by Subject and Activity
+## Sorting the Summary by Activity and ParticipantNumber (Subject)
 
 tidy_data <- tidy_data[order(tidy_data$Activity, tidy_data$ParticipantNumber), ]
-
-View(tidy_data)
 
 ## Saving the Tidy Data Set into a Text File
 
@@ -118,9 +108,4 @@ write.table(tidy_data, file = "Tidy.txt", row.names = FALSE)
 # 06 Reading the Tidy Data Set
 
 tidy <- read.table("Tidy.txt")
-
-
-View(tidy)
-
-
 
